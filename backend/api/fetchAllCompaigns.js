33a7ae645c816ed334eb52b081d547eed0d5668c
Muhammad Arslan <devarslan99@gmail.com>
2024-09-03 19:56:15 +0500

    backend done with top level stats and store it in db

--- backend/api/fetchAllCompaigns.js
+++ backend/api/fetchAllCompaigns.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const Campaign = require('../models/Campaigns');
 const Dailylevel = require('../models/Dailylevel'); // Assuming you have a model for storing campaign stats
+const TopLevelStats = require('../models/TopLevelStats');
+
 
 const FetchAllCampaigns = async (apiKey, user, software) => {
   const url = `https://server.smartlead.ai/api/v1/campaigns/?api_key=${apiKey}`;
@@ -33,6 +35,51 @@ const FetchAllCampaigns = async (apiKey, user, software) => {
         client_id: campaign.client_id
       });
       await newCampaign.save();
+
+
+      const topLevelAnalyticsUrl = `https://server.smartlead.ai/api/v1/campaigns/${campaign.id}/analytics?api_key=${apiKey}`;
+
+      try {
+        const topLevelResponse = await axios.get(topLevelAnalyticsUrl, { headers: { accept: 'application/json' } });
+        const topLevelData = topLevelResponse.data;
+            console.log('topleveldata',topLevelData);
+        // Save top-level analytics data
+        const newTopLevelStats = new TopLevelStats({
+          user_logged_id: user.id,
+          software: software,
+          id: campaign.id,
+          user_id: campaign.user_id,
+          created_at: new Date(topLevelData.created_at),
+          status: topLevelData.status,
+          name: topLevelData.name,
+          sent_count: topLevelData.sent_count,
+          open_count: topLevelData.open_count,
+          click_count: topLevelData.click_count,
+          reply_count: topLevelData.reply_count,
+          block_count: topLevelData.block_count,
+          total_count: topLevelData.total_count,
+          drafted_count: topLevelData.drafted_count,
+          bounce_count: topLevelData.bounce_count,
+          unsubscribed_count: topLevelData.unsubscribed_count,
+          sequence_count: topLevelData.sequence_count,
+          tags: topLevelData.tags,
+          unique_open_count: topLevelData.unique_open_count,
+          unique_click_count: topLevelData.unique_click_count,
+          unique_sent_count: topLevelData.unique_sent_count,
+          client_id: topLevelData.client_id,
+          client_name: topLevelData.client_name,
+          client_email: topLevelData.client_email,
+          parent_campaign_id: topLevelData.parent_campaign_id,
+          campaign_lead_stats: topLevelData.campaign_lead_stats,
+        });
+
+        await newTopLevelStats.save();
+        console.log('done2');
+      } catch (err) {
+        console.error(`Error fetching top-level stats for campaign ${campaign.id}:`, err.message);
+      }
+
+
 
       // Fetch campaign statistics by date range
       const campaignId = campaign.id;
