214c4ab8e4a49f8b6ed0647ea0ae05c2396a36ea
Muhammad Arslan <122959770+devarslan99@users.noreply.github.com>
2024-09-13 20:50:40 +0500

    1.12

--- backend/routes/email.route.js
+++ backend/routes/email.route.js
@@ -11,26 +11,44 @@ console.log('Request Recieved to /client-emails');
 console.log(clientId);
 const Email = require('../models/Email')
 try {
+    // const emails = await Email.find(
+    //     { client_id: null }, // Match documents with the specific client_id
+    //     {
+    //         from_name:1,
+    //         from_email: 1,                     // Include the 'from_email' field
+    //         message_per_day: 1,                 // Include 'message_per_day'
+    //         daily_sent_count: 1,
+    //         warmupStatus: "$warmup_details.status",         
+    //         warmupReputation: "$warmup_details.warmup_reputation",                  // Include 'daily_sent_count'
+    //         warmupBar: {
+    //             $toInt: { 
+    //                 $substr: ["$warmup_details.warmup_reputation", 0, -1] 
+    //             }
+    //     }
+    // }
+    // );
     const emails = await Email.find(
-        { client_id: null }, // Match documents with the specific client_id
+        { client_id: null },
         {
-            from_name:1,
-            from_email: 1,                     // Include the 'from_email' field
-            message_per_day: 1,                 // Include 'message_per_day'
+            from_name: 1,
+            from_email: 1,
+            message_per_day: 1,
             daily_sent_count: 1,
-            warmupStatus: "$warmup_details.status",         
-            warmupReputation: "$warmup_details.warmup_reputation",                  // Include 'daily_sent_count'
-            warmupBar: {
-                $toInt: { 
-                    $substr: ["$warmup_details.warmup_reputation", 0, -1] 
-                }
+            warmupStatus: "$warmup_details.status", // Rename 'warmup_details.status' to 'warmupStatus'
+            warmupReputation: "$warmup_details.warmup_reputation"
         }
-    }
     );
+    
+    const transformedEmails = emails.map(email => ({
+        ...email.toObject(),
+        warmupReputation: parseInt(email.warmup_details.warmup_reputation.split('%')[0], 10)
+    }));
+    
+    console.log(transformedEmails);
 
 
     // Log or return the results
-    console.log("Email List: ", emails);
+    console.log("Email List: ", transformedEmails);
    res.status(200).json(emails)
 } catch (error) {
     console.error("Error fetching email list: ", error);
