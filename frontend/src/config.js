b3296ca1905b907cae3574caf07551303e4f8b01
nomankhalid99 <devarslan99@gmail.com>
2024-10-19 20:28:12 +0500

    change baseurl

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:5000"
-// const BASE_URL = "https://micro-saa-s-project.vercel.app"
+// const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://micro-saa-s-project.vercel.app"
 //working fine
 export {
     BASE_URL
